230fbca7a6a7cd914e501e7d80ccf064878237af
DavidSawicki368 <38538172+DavidSawicki368@users.noreply.github.com>
2018-04-19 10:45:50 -0700

    Add files via upload

---
+++ I2C_Test_2_Master_Servos.ino
@@ -0,0 +1,23 @@
+// Include the required Wire library for I2C<br>
+#include<Wire.h>
+
+int joyVal; // Variable to read the values from the analog pins
+int joyX = A0; // Define joystick pins (Analog)
+
+
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin(); 
+
+}
+
+void loop() {
+
+
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(joyX);        // sends joyVal
+  Wire.endTransmission();    // stop transmitting
+  delay(15);
+}
+
+
---
+++ I2C_Test__1-Slave-Servos_LED.ino
@@ -0,0 +1,31 @@
+#include <Servo.h> // add the servo library
+#include<Wire.h> // Include the required Wire library for I2C<br>
+// Definee the servos
+Servo servo1;
+
+// Variable to read the values from the analog pins
+int joyVal;
+int joyX;
+
+void setup() {
+  // Start the I2C Bus as Slave on address 9
+  Wire.begin(9); 
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+}
+void receiveEvent(int bytes) {
+  joyX = Wire.read();    // read one character from the I2C
+  
+  // Attach servos on pins 3, 5
+  servo1.attach(3);
+}
+
+void loop() {
+  joyVal = analogRead(joyX);
+  joyVal = map (joyVal, 0, 1023, 0, 180); // Servo value between 0-180
+  servo1.write(joyVal); // Set the servo's position according to the joystick value
+  delay(15);
+}
+
+
+
